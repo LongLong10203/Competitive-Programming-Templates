948a2c65393152c7a8e3cd5fb2ad01de917474af
Long <109840350+LongLong10203@users.noreply.github.com>
2024-09-24 22:02:14 +0800

    Update BigInt to include more operators

--- BigInt.cpp
+++ BigInt.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class BigInt {
 private:
     vector<int> digits;
+    vector<bool> binaryDigits;
     bool isNegative = false;
 
     void removeLeadingZeros() {
@@ -54,6 +55,7 @@ private:
             if (a.digits[i] != b.digits[i])
                 return a.digits[i] > b.digits[i];
         }
+
         return false;
     }
 
@@ -63,14 +65,13 @@ public:
     BigInt(const string& num) {
         isNegative = (num[0] == '-');
         int startIndex = isNegative ? 1 : 0;
-        for (int i = num.size() - 1; i >= startIndex; --i) {
+        for (int i = num.size() - 1; i >= startIndex; --i)
             if (isdigit(num[i]))
                 digits.push_back(num[i] - '0');
-        }
         removeLeadingZeros();
     }
 
-    // Addition
+    // Arithmetic operators
     BigInt operator+(const BigInt& other) const {
         if (isNegative == other.isNegative) {
             BigInt result = addAbs(*this, other);
@@ -87,7 +88,6 @@ public:
         }
     }
 
-    // Subtraction
     BigInt operator-(const BigInt& other) const {
         if (isNegative != other.isNegative) {
             BigInt result = addAbs(*this, other);
@@ -104,7 +104,6 @@ public:
         }
     }
 
-    // Multiplication
     BigInt operator*(const BigInt& other) const {
         BigInt result;
         result.digits.assign(digits.size() + other.digits.size(), 0);
@@ -124,23 +123,162 @@ public:
         return result;
     }
 
-    // Comparison
-    bool operator<(const BigInt& other) const {
-        if (isNegative != other.isNegative)
-            return isNegative;
+    BigInt operator/(const BigInt& divisor) const {
+        if (divisor == BigInt("0")) {
+            throw invalid_argument("Division by zero!");
+        }
 
-        if (digits.size() != other.digits.size())
+        BigInt dividend = *this;
+        BigInt quotient("0");
+        BigInt current("0");
+        
+        bool resultNegative = (dividend.isNegative != divisor.isNegative);
+        dividend.isNegative = false;
+        BigInt absDivisor = divisor;
+        absDivisor.isNegative = false;
+        
+        vector<int> result;
+
+        for (int i = dividend.digits.size() - 1; i >= 0; --i) {
+            current.digits.insert(current.digits.begin(), dividend.digits[i]);
+            current.removeLeadingZeros();
+
+            int q = 0;
+            while (current >= absDivisor) {
+                current = current - absDivisor;
+                q++;
+            }
+
+            result.push_back(q);
+        }
+
+        reverse(result.begin(), result.end());
+        quotient.digits = result;
+        quotient.removeLeadingZeros();
+        quotient.isNegative = resultNegative && !(quotient.digits.size() == 1 && quotient.digits[0] == 0);
+
+        return quotient;
+    }
+
+    BigInt operator%(const BigInt& other) const {
+        if (other == BigInt("0")) {
+            throw invalid_argument("Division by zero");
+        }
+
+        BigInt dividend = *this;
+        BigInt divisor = other;
+
+        bool isNegativeResult = false;
+        if (dividend.isNegative) {
+            dividend.isNegative = false;
+            isNegativeResult = true;
+        }
+        if (divisor.isNegative) {
+            divisor.isNegative = false;
+        }
+
+        BigInt quotient = dividend / divisor;
+        BigInt product = quotient * divisor;
+        BigInt remainder = dividend - product;
+
+        if (isNegativeResult) {
+            remainder.isNegative = true;
+        }
+
+        remainder.removeLeadingZeros();
+        return remainder;
+    }
+
+    // Comparison
+    bool operator==(const BigInt& other) const {
+        // Special case: both are zero, return true
+        if (digits.size() == 1 && digits[0] == 0 && other.digits.size() == 1 && other.digits[0] == 0) {
+            return true;
+        }
+        return digits == other.digits && isNegative == other.isNegative;
+    }
+
+    bool operator!=(const BigInt& other) const {
+        return !(*this == other);
+    }
+
+    bool operator<(const BigInt& other) const {
+        if (isNegative != other.isNegative) {
+            return isNegative;
+        }
+
+        if (digits.size() != other.digits.size()) {
             return isNegative ? digits.size() > other.digits.size() : digits.size() < other.digits.size();
+        }
 
         for (int i = digits.size() - 1; i >= 0; --i) {
-            if (digits[i] != other.digits[i])
+            if (digits[i] != other.digits[i]) {
                 return isNegative ? digits[i] > other.digits[i] : digits[i] < other.digits[i];
+            }
         }
+
         return false;
     }
 
-    bool operator==(const BigInt& other) const {
-        return digits == other.digits && isNegative == other.isNegative;
+    bool operator<=(const BigInt& other) const {
+        return *this < other || *this == other;
+    }
+
+    bool operator>(const BigInt& other) const {
+        return !(*this <= other);
+    }
+
+    bool operator>=(const BigInt& other) const {
+        return !(*this < other);
+    }
+
+    // Increment and decrement operators
+    BigInt& operator++() {
+        *this = *this + BigInt("1");
+        return *this;
+    }
+
+    BigInt operator++(int) {
+        BigInt temp = *this;
+        *this = *this + BigInt("1");
+        return temp;
+    }
+
+    BigInt& operator--() {
+        *this = *this - BigInt("1");
+        return *this;
+    }
+
+    BigInt operator--(int) {
+        BigInt temp = *this;
+        *this = *this - BigInt("1");
+        return temp;
+    }
+
+    // I gave up bitwise operators
+
+    // Compound assignment operators
+    BigInt& operator+=(const BigInt& other) {
+        *this = *this + other;
+        return *this;
+    }
+
+    BigInt& operator-=(const BigInt& other) {
+        *this = *this - other;
+        return *this;
+    }
+
+    BigInt& operator*=(const BigInt& other) {
+        *this = *this * other;
+        return *this;
+    }
+
+    BigInt& operator/=(const BigInt& other) {
+        if (other == BigInt("0")) {
+            throw invalid_argument("Division by zero!");
+        }
+        *this = *this / other;
+        return *this;
     }
 
     // Input/Output
