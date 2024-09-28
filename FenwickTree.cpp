5b41ec121342ff3095ba300b812ccd9b2cf609d8
Long <109840350+LongLong10203@users.noreply.github.com>
2024-09-28 16:22:57 +0800

    Add Segment tree for max subarray sum problem

--- FenwickTree.cpp
+++ FenwickTree.cpp
@@ -5,7 +5,7 @@ template <typename T>
 class FenwickTree {
 public:
     FenwickTree() : n(0) {}
- 
+
     FenwickTree(const vector<T>& list) : n(list.size()), tree(n + 1, 0) {
         for (int i = 0; i < n; ++i)
             tree[i + 1] = list[i];
@@ -14,30 +14,30 @@ public:
             if (j <= n) tree[j] += tree[i];
         }
     }
- 
+
     T query(int l, int r) const {
         return prefix_query(r) - (l > 0 ? prefix_query(l - 1) : 0);
     }
- 
+
     void add(int idx, T val) {
         for (++idx; idx <= n; idx += idx & -idx)
             tree[idx] += val;
     }
- 
+
     void set(int idx, T val) {
         T current_val = get(idx);
         T diff = val - current_val;
         add(idx, diff);
     }
- 
+
     T get(int idx) const {
         return query(idx, idx);
     }
- 
+
 private:
     int n;
     vector<T> tree;
- 
+
     T prefix_query(int idx) const {
         T res = 0;
         for (++idx; idx > 0; idx -= idx & -idx)
@@ -45,7 +45,7 @@ private:
         return res;
     }
 }; // * Note: 0-based index
- 
+
 int main() {
     
 }
---
+++ SegmentTreeMaxSubarraySum.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class SegmentTree {
+private:
+    struct Node {
+        T maxSum, prefixSum, suffixSum, totalSum;
+
+        Node(T value = 0) {
+            totalSum = value;
+            prefixSum = suffixSum = maxSum = max(T(0), value);
+        }
+    };
+
+    vector<Node> tree;
+    vector<T> data;
+    int n;
+
+    Node combine(Node left, Node right) {
+        Node res;
+        res.totalSum = left.totalSum + right.totalSum;
+        res.prefixSum = max(left.prefixSum, left.totalSum + right.prefixSum);
+        res.suffixSum = max(right.suffixSum, right.totalSum + left.suffixSum);
+        res.maxSum = max({left.maxSum, right.maxSum, left.suffixSum + right.prefixSum});
+        return res;
+    }
+
+    void build(int idx, int l, int r) {
+        if (l == r) {
+            tree[idx] = Node(data[l]);
+            return;
+        }
+        int mid = (l + r) / 2;
+        build(2 * idx, l, mid);
+        build(2 * idx + 1, mid + 1, r);
+        tree[idx] = combine(tree[2 * idx], tree[2 * idx + 1]);
+    }
+
+    void update(int idx, int l, int r, int pos, T val) {
+        if (l == r) {
+            tree[idx] = Node(val);
+            return;
+        }
+        int mid = (l + r) / 2;
+        if (pos <= mid)
+            update(2 * idx, l, mid, pos, val);
+        else
+            update(2 * idx + 1, mid + 1, r, pos, val);
+        tree[idx] = combine(tree[2 * idx], tree[2 * idx + 1]);
+    }
+
+    Node query(int idx, int l, int r, int left, int right) {
+        if (left == l && right == r)
+            return tree[idx];
+        int mid = (l + r) / 2;
+        if (right <= mid)
+            return query(2 * idx, l, mid, left, right);
+        else if (left > mid)
+            return query(2 * idx + 1, mid + 1, r, left, right);
+        else
+            return combine(
+                query(2 * idx, l, mid, left, mid),
+                query(2 * idx + 1, mid + 1, r, mid + 1, right)
+            );
+    }
+
+public:
+    SegmentTree(const vector<T>& input_data) {
+        data = input_data;
+        n = data.size();
+        tree.resize(4 * n);
+        build(1, 0, n - 1);
+    }
+
+    void update(int pos, T val) {
+        update(1, 0, n - 1, pos, val);
+    }
+
+    T query(int l, int r) {
+        Node result = query(1, 0, n - 1, l, r);
+        return result.maxSum;
+    }
+}; // * Note: 0-based index
+
+int main() {
+    
+}
