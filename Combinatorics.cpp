ed2a8446185d82ce629d3d645aa5ca8bc675509e
Long <109840350+LongLong10203@users.noreply.github.com>
2024-09-29 13:24:14 +0800

    Update Combinatorics

--- Combinatorics.cpp
+++ Combinatorics.cpp
@@ -11,7 +11,9 @@ private:
     void precompute(int maxN) {
         factMemo[0] = 1;
         for (int i = 1; i <= maxN; ++i)
-            factMemo[i] = (factMemo[i - 1] * i) % MOD;
+            factMemo[i] = (MOD == -1) ? factMemo[i - 1] * i : (factMemo[i - 1] * i) % MOD;
+        if (MOD == -1)
+            return;
         invFactMemo[maxN] = power(factMemo[maxN], MOD - 2);
         for (int i = maxN - 1; i >= 0; --i)
             invFactMemo[i] = (invFactMemo[i + 1] * (i + 1)) % MOD;
@@ -29,19 +31,25 @@ private:
     }
 
 public:
-    Combinatorics(int maxN, T mod = 1) : MOD(mod) {
+    Combinatorics(int maxN, T mod=-1) : MOD(mod) {
         factMemo.resize(maxN + 1);
-        invFactMemo.resize(maxN + 1);
+        if (mod != -1)
+            invFactMemo.resize(maxN + 1);
         precompute(maxN);
     }
 
     T factorial(int n) {
+        if (n < 0) throw invalid_argument("n must be non-negative");
         return factMemo[n];
     }
 
     T nCr(int n, int r) {
         if (r > n) throw invalid_argument("r > n");
-        return (factMemo[n] * invFactMemo[r] % MOD * invFactMemo[n - r] % MOD) % MOD;
+        if (r == 0 || r == n) return 1;
+        if (MOD == -1)
+            return factorial(n) / (factorial(r) * factorial(n - r));
+        else
+            return (factMemo[n] * invFactMemo[r] % MOD * invFactMemo[n - r] % MOD) % MOD;
     }
 };
 
