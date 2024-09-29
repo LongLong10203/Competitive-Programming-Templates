ca417d234a64ef1216e4f00587df6b3f688e7918
Long <109840350+LongLong10203@users.noreply.github.com>
2024-09-29 14:06:09 +0800

    Update Combinatorics

--- Combinatorics.cpp
+++ Combinatorics.cpp
@@ -1,49 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template<typename T>
 class Combinatorics {
 private:
-    vector<T> factMemo;
-    vector<T> invFactMemo;
-    T MOD;
+    int MOD;
+    vector<long long> fact, inv_fact;
 
-    void precompute(int maxN) {
-        factMemo[0] = 1;
-        for (int i = 1; i <= maxN; ++i)
-            factMemo[i] = (factMemo[i - 1] * i) % MOD;
-        invFactMemo[maxN] = power(factMemo[maxN], MOD - 2);
-        for (int i = maxN - 1; i >= 0; --i)
-            invFactMemo[i] = (invFactMemo[i + 1] * (i + 1)) % MOD;
+    void precompute(int n) {
+        fact[0] = 1;
+        for (int i = 1; i <= n; i++)
+            fact[i] = fact[i - 1] * i % MOD;
+        inv_fact[n] = mod_pow(fact[n], MOD - 2);
+        for (int i = n - 1; i >= 0; i--)
+            inv_fact[i] = inv_fact[i + 1] * (i + 1) % MOD;
     }
 
-    T power(T x, T y) {
-        T res = 1;
-        x = x % MOD;
-        while (y > 0) {
-            if (y & 1) res = (res * x) % MOD;
-            y = y >> 1;
-            x = (x * x) % MOD;
+public:
+    Combinatorics(int n, int mod) : MOD(mod), fact(n + 1), inv_fact(n + 1) {
+        precompute(n);
+    }
+
+    long long mod_pow(long long base, long long exp) {
+        long long res = 1;
+        while (exp > 0) {
+            if (exp & 1)
+                res = (res * base) % MOD;
+            base = (base * base) % MOD;
+            exp >>= 1;
         }
         return res;
     }
 
-public:
-    Combinatorics(int maxN, T mod) : MOD(mod) {
-        factMemo.resize(maxN + 1);
-        invFactMemo.resize(maxN + 1);
-        precompute(maxN);
+    long long nCr(int n, int k) {
+        if (k > n || k < 0) return 0;
+        return fact[n] * inv_fact[k] % MOD * inv_fact[n - k] % MOD;
     }
 
-    T factorial(int n) {
-        return factMemo[n];
+    long long nPr(int n, int k) {
+        if (k > n || k < 0) return 0;
+        return fact[n] * inv_fact[n - k] % MOD;
     }
 
-    T nCr(int n, int r) {
-        if (r > n) throw invalid_argument("r > n");
-        return (factMemo[n] * invFactMemo[r] % MOD * invFactMemo[n - r] % MOD) % MOD;
+    long long factorial(int n) {
+        return fact[n];
     }
-};
+}; // * Note: initialise with maxN
 
 int main() {
     
