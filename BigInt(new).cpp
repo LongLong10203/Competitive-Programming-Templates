d9ffe7bfe0bffe4a9e0396e9c244b15fa0cf75a9
Long <109840350+LongLong10203@users.noreply.github.com>
2025-01-20 21:22:48 +0800

    rewrite BigInt class
    used github copilot for:
    - chatgpt o1 preview
    - chatgpt 4o

---
+++ BigInt(new).cpp
@@ -0,0 +1,230 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+static const double PI = acos(-1);
+
+void fft(vector<complex<double>> &a, bool invert) {
+    int n = (int)a.size();
+    for(int i = 1, j = 0; i < n; i++){
+        int bit = n >> 1;
+        for(; j & bit; bit >>= 1) j ^= bit;
+        j ^= bit;
+        if(i < j) swap(a[i], a[j]);
+    }
+    for(int len = 2; len <= n; len <<= 1){
+        double ang = 2*PI/len * (invert ? -1 : 1);
+        complex<double> wlen(cos(ang), sin(ang));
+        for(int i = 0; i < n; i += len){
+            complex<double> w(1);
+            for(int j = 0; j < len/2; j++){
+                complex<double> u = a[i+j], v = a[i+j+len/2]*w;
+                a[i+j] = u + v;
+                a[i+j+len/2] = u - v;
+                w *= wlen;
+            }
+        }
+    }
+    if(invert){
+        for(auto &x : a) x /= n;
+    }
+}
+
+string multiplyFFT(const string &num1, const string &num2) {
+    bool neg = false;
+    int start1 = 0, start2 = 0;
+    if(num1[0] == '-') { neg = !neg; start1 = 1; }
+    if(num2[0] == '-') { neg = !neg; start2 = 1; }
+
+    vector<int> a, b;
+    for(int i = (int)num1.size()-1; i >= start1; i--) a.push_back(num1[i]-'0');
+    for(int i = (int)num2.size()-1; i >= start2; i--) b.push_back(num2[i]-'0');
+
+    int n = 1;
+    while(n < (int)(a.size() + b.size())) n <<= 1;
+    vector<complex<double>> fa(n), fb(n);
+    for(size_t i = 0; i < a.size(); i++) fa[i] = a[i];
+    for(size_t i = 0; i < b.size(); i++) fb[i] = b[i];
+
+    fft(fa, false); fft(fb, false);
+    for(int i = 0; i < n; i++) fa[i] *= fb[i];
+    fft(fa, true);
+
+    vector<int> result(n);
+    for(int i = 0; i < n; i++){
+        long long val = (long long)llround(fa[i].real());
+        if(val < 0) val = 0;
+        result[i] = (int)val;
+    }
+    for(int i = 0; i < n-1; i++){
+        result[i+1] += result[i]/10;
+        result[i] %= 10;
+    }
+    while(result.size()>1 && result.back()==0) result.pop_back();
+
+    string ans;
+    if(neg && !(result.size()==1 && result[0] == 0)) ans.push_back('-');
+    for(int i = (int)result.size()-1; i >= 0; i--) ans.push_back((char)(result[i]+'0'));
+    return ans;
+}
+
+class BigInt {
+private:
+    string number;
+
+    void normalize() {
+        if(number.size() > 1) {
+            bool neg = (number[0] == '-');
+            int start = neg?1:0;
+            while(number.size() > start+1 && number[start] == '0') number.erase(start,1);
+            if(number == "-") number = "0";
+        }
+    }
+
+public:
+    BigInt() : number("0") {}
+    BigInt(const string &s) : number(s.empty()?"0":s) { normalize(); }
+    BigInt(long long v) { number = to_string(v); normalize(); }
+    BigInt(int v) { number = to_string(v); normalize(); }
+
+    BigInt& operator=(const string &s){ number = s; normalize(); return *this; }
+    BigInt& operator=(long long v){ number = to_string(v); normalize(); return *this; }
+    BigInt& operator=(int v){ number = to_string(v); normalize(); return *this; }
+
+    bool operator==(const BigInt &other) const {
+        return number == other.number;
+    }
+    bool operator!=(const BigInt &other) const { return !(*this == other); }
+    bool operator<(const BigInt &other) const {
+        if(number[0] == '-' && other.number[0] != '-') return true;
+        if(number[0] != '-' && other.number[0] == '-') return false;
+
+        bool neg = (number[0] == '-');
+        string a = neg ? number.substr(1) : number;
+        string b = neg ? other.number.substr(1) : other.number;
+
+        if(a.size() != b.size()) {
+            return (a.size() < b.size()) ^ neg;
+        }
+        return (a < b) ^ neg;
+    }
+    bool operator>(const BigInt &other) const { return other < *this; }
+    bool operator<=(const BigInt &other) const { return !(other < *this); }
+    bool operator>=(const BigInt &other) const { return !(*this < other); }
+
+    template <typename T>
+    bool operator==(T other) const { return *this == BigInt(other); }
+    template <typename T>
+    bool operator!=(T other) const { return !(*this == other); }
+    template <typename T>
+    bool operator<(T other) const { return *this < BigInt(other); }
+    template <typename T>
+    bool operator>(T other) const { return other < *this; }
+    template <typename T>
+    bool operator<=(T other) const { return !(other < *this); }
+    template <typename T>
+    bool operator>=(T other) const { return !(*this < other); }
+
+    template <typename T>
+    friend bool operator==(T lhs, const BigInt &rhs) { return BigInt(lhs) == rhs; }
+    template <typename T>
+    friend bool operator!=(T lhs, const BigInt &rhs) { return BigInt(lhs) != rhs; }
+    template <typename T>
+    friend bool operator<(T lhs, const BigInt &rhs) { return BigInt(lhs) < rhs; }
+    template <typename T>
+    friend bool operator>(T lhs, const BigInt &rhs) { return rhs < lhs; }
+    template <typename T>
+    friend bool operator<=(T lhs, const BigInt &rhs) { return !(rhs < lhs); }
+    template <typename T>
+    friend bool operator>=(T lhs, const BigInt &rhs) { return !(lhs < rhs); }
+
+    BigInt operator-() const {
+        BigInt r = *this;
+        if(r.number != "0") {
+            if(r.number[0] == '-') r.number.erase(0,1);
+            else r.number.insert(r.number.begin(), '-');
+        }
+        return r;
+    }
+
+    static string addStrings(const string &a, const string &b) {
+        string res;
+        int carry = 0, i = (int)a.size()-1, j = (int)b.size()-1;
+        while(i>=0 || j>=0 || carry){
+            int x = (i>=0 ? a[i--]-'0' : 0);
+            int y = (j>=0 ? b[j--]-'0' : 0);
+            int sum = x + y + carry;
+            carry = sum/10; sum%=10;
+            res.push_back((char)('0'+sum));
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+
+    static string subStrings(const string &a, const string &b) {
+        string res;
+        int i = (int)a.size()-1, j = (int)b.size()-1, carry = 0;
+        while(i>=0){
+            int x = a[i--]-'0' - carry;
+            int y = (j>=0 ? b[j--]-'0' : 0);
+            if(x < y){ x += 10; carry = 1;} else carry = 0;
+            res.push_back((char)('0'+(x-y)));
+        }
+        while(res.size()>1 && res.back()=='0') res.pop_back();
+        reverse(res.begin(), res.end());
+        return res;
+    }
+
+    BigInt operator+(const BigInt &other) const {
+        if(number[0] == '-' && other.number[0] == '-') {
+            return -BigInt(addStrings(number.substr(1), other.number.substr(1)));
+        } else if(number[0] == '-') {
+            return BigInt(other) - BigInt(number.substr(1));
+        } else if(other.number[0] == '-') {
+            return *this - BigInt(other.number.substr(1));
+        }
+        return BigInt(addStrings(number, other.number));
+    }
+
+    BigInt operator-(const BigInt &other) const {
+        if(other.number[0] == '-') {
+            return *this + BigInt(other.number.substr(1));
+        } else if(number[0] == '-') {
+            return -BigInt(addStrings(number.substr(1), other.number));
+        }
+        if(*this < other){
+            return -BigInt(subStrings(other.number, number));
+        } else {
+            return BigInt(subStrings(number, other.number));
+        }
+    }
+
+    BigInt operator*(const BigInt &other) const {
+        return BigInt(multiplyFFT(number, other.number));
+    }
+
+    BigInt& operator+=(const BigInt &other){ *this = *this + other; return *this; }
+    BigInt& operator-=(const BigInt &other){ *this = *this - other; return *this; }
+    BigInt& operator*=(const BigInt &other){ *this = *this * other; return *this; }
+
+    BigInt& operator++() { *this += 1; return *this; }
+    BigInt operator++(int) { BigInt tmp = *this; ++(*this); return tmp; }
+    BigInt& operator--() { *this -= 1; return *this; }
+    BigInt operator--(int) { BigInt tmp = *this; --(*this); return tmp; }
+
+    friend ostream& operator<<(ostream &out, const BigInt &val) {
+        out << val.number; 
+        return out;
+    }
+
+    friend istream& operator>>(istream &in, BigInt &val) {
+        in >> val.number;
+        val.normalize();
+        return in;
+    }
+};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    
+}
