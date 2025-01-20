39f6931881f985a19def41b6ff395c11280fa3e4
Long <109840350+LongLong10203@users.noreply.github.com>
2025-01-20 22:18:47 +0800

    reorder functions in BigInt
    add toInt, toLL, toString methods, make helper functions private in class

--- BigInt(new).cpp
+++ BigInt(new).cpp
@@ -1,72 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-static const double PI = acos(-1);
-
-void fft(vector<complex<double>> &a, bool invert) {
-    int n = (int)a.size();
-    for(int i = 1, j = 0; i < n; i++){
-        int bit = n >> 1;
-        for(; j & bit; bit >>= 1) j ^= bit;
-        j ^= bit;
-        if(i < j) swap(a[i], a[j]);
-    }
-    for(int len = 2; len <= n; len <<= 1){
-        double ang = 2*PI/len * (invert ? -1 : 1);
-        complex<double> wlen(cos(ang), sin(ang));
-        for(int i = 0; i < n; i += len){
-            complex<double> w(1);
-            for(int j = 0; j < len/2; j++){
-                complex<double> u = a[i+j], v = a[i+j+len/2]*w;
-                a[i+j] = u + v;
-                a[i+j+len/2] = u - v;
-                w *= wlen;
-            }
-        }
-    }
-    if(invert){
-        for(auto &x : a) x /= n;
-    }
-}
-
-string multiplyFFT(const string &num1, const string &num2) {
-    bool neg = false;
-    int start1 = 0, start2 = 0;
-    if(num1[0] == '-') { neg = !neg; start1 = 1; }
-    if(num2[0] == '-') { neg = !neg; start2 = 1; }
-
-    vector<int> a, b;
-    for(int i = (int)num1.size()-1; i >= start1; i--) a.push_back(num1[i]-'0');
-    for(int i = (int)num2.size()-1; i >= start2; i--) b.push_back(num2[i]-'0');
-
-    int n = 1;
-    while(n < (int)(a.size() + b.size())) n <<= 1;
-    vector<complex<double>> fa(n), fb(n);
-    for(size_t i = 0; i < a.size(); i++) fa[i] = a[i];
-    for(size_t i = 0; i < b.size(); i++) fb[i] = b[i];
-
-    fft(fa, false); fft(fb, false);
-    for(int i = 0; i < n; i++) fa[i] *= fb[i];
-    fft(fa, true);
-
-    vector<int> result(n);
-    for(int i = 0; i < n; i++){
-        long long val = (long long)llround(fa[i].real());
-        if(val < 0) val = 0;
-        result[i] = (int)val;
-    }
-    for(int i = 0; i < n-1; i++){
-        result[i+1] += result[i]/10;
-        result[i] %= 10;
-    }
-    while(result.size()>1 && result.back()==0) result.pop_back();
-
-    string ans;
-    if(neg && !(result.size()==1 && result[0] == 0)) ans.push_back('-');
-    for(int i = (int)result.size()-1; i >= 0; i--) ans.push_back((char)(result[i]+'0'));
-    return ans;
-}
-
 class BigInt {
 private:
     string number;
@@ -74,25 +8,115 @@ private:
     void normalize() {
         if(number.size() > 1) {
             bool neg = (number[0] == '-');
-            int start = neg?1:0;
-            while(number.size() > start+1 && number[start] == '0') number.erase(start,1);
+            int start = neg ? 1 : 0;
+            while(number.size() > start + 1 && number[start] == '0') number.erase(start, 1);
             if(number == "-") number = "0";
         }
     }
 
+    static void fft(vector<complex<double>> &a, bool invert) {
+        int n = (int)a.size();
+        for(int i = 1, j = 0; i < n; i++){
+            int bit = n >> 1;
+            for(; j & bit; bit >>= 1) j ^= bit;
+            j ^= bit;
+            if(i < j) swap(a[i], a[j]);
+        }
+        for(int len = 2; len <= n; len <<= 1){
+            double ang = 2*acos(-1)/len * (invert ? -1 : 1);
+            complex<double> wlen(cos(ang), sin(ang));
+            for(int i = 0; i < n; i += len){
+                complex<double> w(1);
+                for(int j = 0; j < len/2; j++){
+                    complex<double> u = a[i+j], v = a[i+j+len/2]*w;
+                    a[i+j] = u + v;
+                    a[i+j+len/2] = u - v;
+                    w *= wlen;
+                }
+            }
+        }
+        if(invert){
+            for(auto &x : a) x /= n;
+        }
+    }
+
+    static string multiplyFFT(const string &num1, const string &num2) {
+        bool neg = false;
+        int start1 = 0, start2 = 0;
+        if(num1[0] == '-') { neg = !neg; start1 = 1; }
+        if(num2[0] == '-') { neg = !neg; start2 = 1; }
+
+        vector<int> a, b;
+        for(int i = (int)num1.size()-1; i >= start1; i--) a.push_back(num1[i]-'0');
+        for(int i = (int)num2.size()-1; i >= start2; i--) b.push_back(num2[i]-'0');
+
+        int n = 1;
+        while(n < (int)(a.size() + b.size())) n <<= 1;
+        vector<complex<double>> fa(n), fb(n);
+        for(size_t i = 0; i < a.size(); i++) fa[i] = a[i];
+        for(size_t i = 0; i < b.size(); i++) fb[i] = b[i];
+
+        fft(fa, false); fft(fb, false);
+        for(int i = 0; i < n; i++) fa[i] *= fb[i];
+        fft(fa, true);
+
+        vector<int> result(n);
+        for(int i = 0; i < n; i++){
+            long long val = (long long)llround(fa[i].real());
+            if(val < 0) val = 0;
+            result[i] = (int)val;
+        }
+        for(int i = 0; i < n-1; i++){
+            result[i+1] += result[i]/10;
+            result[i] %= 10;
+        }
+        while(result.size()>1 && result.back()==0) result.pop_back();
+
+        string ans;
+        if(neg && !(result.size()==1 && result[0] == 0)) ans.push_back('-');
+        for(int i = (int)result.size()-1; i >= 0; i--) ans.push_back((char)(result[i]+'0'));
+        return ans;
+    }
+
+    static string addStrings(const string &a, const string &b) {
+        string res;
+        int carry = 0, i = (int)a.size() - 1, j = (int)b.size() - 1;
+        while(i >= 0 || j >= 0 || carry) {
+            int x = (i >= 0 ? a[i--] - '0' : 0);
+            int y = (j >= 0 ? b[j--] - '0' : 0);
+            int sum = x + y + carry;
+            carry = sum / 10; sum %= 10;
+            res.push_back((char)('0' + sum));
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+
+    static string subStrings(const string &a, const string &b) {
+        string res;
+        int i = (int)a.size() - 1, j = (int)b.size() - 1, carry = 0;
+        while(i >= 0) {
+            int x = a[i--] - '0' - carry;
+            int y = (j >= 0 ? b[j--] - '0' : 0);
+            if(x < y) { x += 10; carry = 1; } else carry = 0;
+            res.push_back((char)('0' + (x - y)));
+        }
+        while(res.size() > 1 && res.back() == '0') res.pop_back();
+        reverse(res.begin(), res.end());
+        return res;
+    }
+
 public:
     BigInt() : number("0") {}
-    BigInt(const string &s) : number(s.empty()?"0":s) { normalize(); }
+    BigInt(const string &s) : number(s.empty() ? "0" : s) { normalize(); }
     BigInt(long long v) { number = to_string(v); normalize(); }
     BigInt(int v) { number = to_string(v); normalize(); }
 
-    BigInt& operator=(const string &s){ number = s; normalize(); return *this; }
-    BigInt& operator=(long long v){ number = to_string(v); normalize(); return *this; }
-    BigInt& operator=(int v){ number = to_string(v); normalize(); return *this; }
+    BigInt& operator=(const string &s) { number = s; normalize(); return *this; }
+    BigInt& operator=(long long v) { number = to_string(v); normalize(); return *this; }
+    BigInt& operator=(int v) { number = to_string(v); normalize(); return *this; }
 
-    bool operator==(const BigInt &other) const {
-        return number == other.number;
-    }
+    bool operator==(const BigInt &other) const { return number == other.number; }
     bool operator!=(const BigInt &other) const { return !(*this == other); }
     bool operator<(const BigInt &other) const {
         if(number[0] == '-' && other.number[0] != '-') return true;
@@ -140,38 +164,10 @@ public:
     BigInt operator-() const {
         BigInt r = *this;
         if(r.number != "0") {
-            if(r.number[0] == '-') r.number.erase(0,1);
+            if(r.number[0] == '-') r.number.erase(0, 1);
             else r.number.insert(r.number.begin(), '-');
         }
         return r;
-    }
-
-    static string addStrings(const string &a, const string &b) {
-        string res;
-        int carry = 0, i = (int)a.size()-1, j = (int)b.size()-1;
-        while(i>=0 || j>=0 || carry){
-            int x = (i>=0 ? a[i--]-'0' : 0);
-            int y = (j>=0 ? b[j--]-'0' : 0);
-            int sum = x + y + carry;
-            carry = sum/10; sum%=10;
-            res.push_back((char)('0'+sum));
-        }
-        reverse(res.begin(), res.end());
-        return res;
-    }
-
-    static string subStrings(const string &a, const string &b) {
-        string res;
-        int i = (int)a.size()-1, j = (int)b.size()-1, carry = 0;
-        while(i>=0){
-            int x = a[i--]-'0' - carry;
-            int y = (j>=0 ? b[j--]-'0' : 0);
-            if(x < y){ x += 10; carry = 1;} else carry = 0;
-            res.push_back((char)('0'+(x-y)));
-        }
-        while(res.size()>1 && res.back()=='0') res.pop_back();
-        reverse(res.begin(), res.end());
-        return res;
     }
 
     BigInt operator+(const BigInt &other) const {
@@ -191,7 +187,7 @@ public:
         } else if(number[0] == '-') {
             return -BigInt(addStrings(number.substr(1), other.number));
         }
-        if(*this < other){
+        if(*this < other) {
             return -BigInt(subStrings(other.number, number));
         } else {
             return BigInt(subStrings(number, other.number));
@@ -202,9 +198,9 @@ public:
         return BigInt(multiplyFFT(number, other.number));
     }
 
-    BigInt& operator+=(const BigInt &other){ *this = *this + other; return *this; }
-    BigInt& operator-=(const BigInt &other){ *this = *this - other; return *this; }
-    BigInt& operator*=(const BigInt &other){ *this = *this * other; return *this; }
+    BigInt& operator+=(const BigInt &other) { *this = *this + other; return *this; }
+    BigInt& operator-=(const BigInt &other) { *this = *this - other; return *this; }
+    BigInt& operator*=(const BigInt &other) { *this = *this * other; return *this; }
 
     BigInt& operator++() { *this += 1; return *this; }
     BigInt operator++(int) { BigInt tmp = *this; ++(*this); return tmp; }
@@ -272,10 +268,20 @@ public:
         *this = *this % other;
         return *this;
     }
+
+    int toInt() const {
+        return stoi(number);
+    }
+
+    long long toLL() const {
+        return stoll(number);
+    }
+
+    string toString() const {
+        return number;
+    }
 };
 
 int main() {
-    cin.tie(0)->sync_with_stdio(0);
-
     
 }
