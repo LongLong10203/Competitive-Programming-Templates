4b5c818dff2889112b097c9731ab2a023b7e0237
Long <109840350+LongLong10203@users.noreply.github.com>
2025-02-09 11:32:25 +0800

    modify fast exp

--- fastexp.cpp
+++ fastexp.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 const int MOD = 1e9 + 7;
 
-template <typename T>
-T fastexp(T base, T exp, T mod = MOD) {
-    T res = 1;
+ll fastexp(ll base, ll exp, ll mod = MOD) {
+    ll res = 1;
     while (exp > 0) {
-        if (exp & 1) res = (res * base) % MOD;
-        base = (base * base) % MOD;
+        if (exp & 1) res = (res * base) % mod;
+        base = (base * base) % mod;
         exp >>= 1;
     }
     return res;
@@ -17,5 +17,5 @@ T fastexp(T base, T exp, T mod = MOD) {
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
-    int b, p, m;
+    
 }
