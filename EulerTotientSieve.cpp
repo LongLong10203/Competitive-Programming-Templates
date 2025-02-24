89c87c17cda58085fb32cb2e7eb5bc8ef8702c74
Long <109840350+LongLong10203@users.noreply.github.com>
2025-02-24 20:52:17 +0800

    refine sieves

--- EulerTotientSieve.cpp
+++ EulerTotientSieve.cpp
@@ -1,16 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> tot(int n) {
-    vector<int> phi(n + 1);
-    for (int i = 1; i <= n; i++)
-        phi[i] = i;
-    for (int p = 2; p <= n; p++)
-        if (phi[p] == p)
-            for (int k = p; k <= n; k += p)
-                phi[k] -= phi[k] / p;
-    return phi;
-}
+struct EulerTotientSieve {
+    vector<int> tot(int n) {
+        vector<int> phi(n + 1);
+        for (int i = 1; i <= n; i++)
+            phi[i] = i;
+        for (int p = 2; p <= n; p++)
+            if (phi[p] == p)
+                for (int k = p; k <= n; k += p)
+                    phi[k] -= phi[k] / p;
+        return phi;
+    }
+    vector<int> phi;
+    EulerTotientSieve(int n) : phi(tot(n)) {}
+    int operator[](int i) { return phi[i]; }
+};
 
 int main() {
 
--- SieveOfEratosthenes.cpp
+++ SieveOfEratosthenes.cpp
@@ -1,15 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<bool> sieve(int n) {
-    vector<bool> prime(n + 1, true);
-    prime[0] = prime[1] = false;
-    for (int p = 2; p * p <= n; ++p)
-        if (prime[p])
-            for (int i = p * p; i <= n; i += p)
-                prime[i] = false;
-    return prime;
-}
+struct Sieve {
+    vector<bool> sieve(int n) {
+        vector<bool> prime(n + 1, true);
+        prime[0] = prime[1] = false;
+        for (int p = 2; p * p <= n; ++p)
+            if (prime[p])
+                for (int i = p * p; i <= n; i += p)
+                    prime[i] = false;
+        return prime;
+    }
+    vector<bool> prime;
+    Sieve(int n) : prime(sieve(n)) {}
+    bool isPrime(int n) {
+        return prime[n];
+    }
+};
 
 int main() {
     
